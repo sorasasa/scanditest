e4e18bb2cccd61ec18bfbaca24570f0b22948a9e
Sarah <101598732+sorasasa@users.noreply.github.com>
2023-06-13 12:02:18 -0300

    att

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+
+  publicPath: athprocess.env.NODE_ENV === "production" ? "/scanditest/" : "/",
   transpileDependencies: true
 })
