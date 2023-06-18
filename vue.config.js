763f179574bcf486ba6f7603166f61580ce710a6
Sarah <101598732+sorasasa@users.noreply.github.com>
2023-06-17 22:55:59 -0300

    proxyda

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,6 @@ module.exports = defineConfig({
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization" }
     },
-    //proxy:"http://localhost:3000/products"
+    proxy:"http://localhost:3000/products"
   
 })
