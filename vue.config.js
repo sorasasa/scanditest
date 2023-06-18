6a13163bf781fbb99161736d0016caa2487973bd
Sarah <101598732+sorasasa@users.noreply.github.com>
2023-06-17 23:02:21 -0300

    lowside

--- vue.config.js
+++ vue.config.js
@@ -7,9 +7,9 @@ module.exports = defineConfig({
   
   devServer: { /**important*/
     before: historyApiFallback,
-    headers: { "Access-Control-Allow-Origin": "*",
+    /*headers: { "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization" },
+    "Access-Control-Allow-Headers": "Content-Type, Authorization" },*/
     proxy:"http://localhost:3000/products"
     },
   
