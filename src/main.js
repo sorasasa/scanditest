8c26ec5359874f4afe6323d475a0b772f1d00243
Sarah <101598732+sorasasa@users.noreply.github.com>
2023-06-22 00:58:40 -0300

    ??

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios' //
-/*import VueResource from 'vue-resource';
-
-Vue.use(VueResource);
-
-Vue.http.options.emulateJSON = true;
-Vue.http.options.crossOrigin = true;*/
 
 axios.defaults.withCredentials = true;//
 
