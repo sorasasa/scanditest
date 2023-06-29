4bdf5b6a8b96d760c437eeb59c7e30a08d7add17
Sarah <101598732+sorasasa@users.noreply.github.com>
2023-06-29 16:17:37 -0300

    teste24

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios' 
+import axios from 'axios' /** 
 import { ValidationObserver, ValidationProvider } from 'vee-validate';
 
 Vue.component('ValidationObserver', ValidationObserver);
 Vue.component('ValidationProvider', ValidationProvider);
-
+*/
 axios.defaults.withCredentials = true;
 
 createApp(App).use(router).mount('#app')
